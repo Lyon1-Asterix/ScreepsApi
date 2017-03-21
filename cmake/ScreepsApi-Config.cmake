f56ce0fa90e32fd5c5cec9d78aff10ee83195ca5
Erwan Guillou <erwan.guillou@liris.cnrs.fr>
2017-03-21 12:34:14 +0100

    cmake config file

--- cmake/ScreepsApi-Config.cmake
+++ cmake/ScreepsApi-Config.cmake
@@ -1,5 +1,5 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(ScreepsApi_INCLUDE_DIRS "${SELF_DIR}/../../include/ScreepsApi" ABSOLUTE)
+get_filename_component(ScreepsApi_INCLUDE_DIRS "${SELF_DIR}/../../include" ABSOLUTE)
 get_filename_component(ScreepsApi_LIBRARY_DIRS "${SELF_DIR}/../lib" ABSOLUTE)
 set(ScreepsApi_LIBRARIES "${CMAKE_SHARED_LIBRARY_PREFIX}ScreepsApi${CMAKE_SHARED_LIBRARY_SUFFIX}")
